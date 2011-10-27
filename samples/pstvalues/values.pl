1da96b03e1ab3a3a30012ff6c1b0dbce10cfce30
terrymah <terry.mahaffey@gmail.com>
2011-10-26 22:22:10 -0700

    Fixup pstvalues sample

--- samples/pstvalues/values.pl
+++ samples/pstvalues/values.pl
@@ -29,8 +29,8 @@
 #   definition. We would pick up the typedef as well, and also consider B a 
 #   valid type. 
 #
-#   We would pick up the size of the class, the offset of name1 and name2,
-#   and the value of variable in the output file.
+#   We would pick up the size of the class, the offset of name2 and name3,
+#   and the value of name1 in the output file.
 #
 # enum enumname
 # {
@@ -63,9 +63,16 @@ my %valid_types = (
     'double' => undef,
     'long double' => undef,
     'wchar_t' => undef,
+	'size_t' => undef,
+	'boost::uint32_t' => undef,
+	'boost::uint32_t' => undef,
+	'boost::int32_t' => undef,
+	'boost::uint64_t' => undef,
+	'boost::int64_t' => undef,
+	'boost::uint8_t' => undef,
+	'boost::uint16_t' => undef,
 );
-
-my $valid_types_regex = '(' . join('|',keys %valid_types) . ')';
+my %valid_types_local = ();
 
 # output the header
 print "//\n";
@@ -96,12 +103,15 @@ my $template_param;
 my $non_trivial;
 my $param_name;
 
+my $varregex = "[A-Za-z_][A-Za-z_0-9]*";
+
 foreach (0 ... $#ARGV) {
     open inputfile, "<", $ARGV[$_];
 
     # This loop is at the global scope
 
     while(<inputfile>) {
+		my $valid_types_regex = '(' . join('|',keys %valid_types) . ')';
 
         # skip all comments
         if(m#^//#) {
@@ -109,126 +119,162 @@ foreach (0 ... $#ARGV) {
         }
         
         # global typedef, add to our array
-        if(/typedef\s$valid_types_regex\s(\w*);/) {
+        if(/typedef\s$valid_types_regex\s($varregex);/) {
             if(exists $valid_types{$1}) {
                 $valid_types{$2} = undef;
-                $valid_types_regex = '(' . join('|',keys %valid_types) . ')';
                 print "\t{ L\"sizeof($2)\", sizeof($2) },\n";
             }
         }
 
         # global const variable, print it out
-        if(/const\s$valid_types_regex (\w*) =/) {
+        elsif(/const\s$valid_types_regex ($varregex) =/) {
             print "\t{ L\"$2\", $2 },\n";
         }
 
         # template definition
-        if(/template<typename (\w*)>/) {
+        elsif(/template<typename ($varregex)>/) {
             $template_param = $1;
             if($template_param ne "T") {
                 while(<inputfile>) {
-                    if(/};/) {
+                    if(/^}.*;/) {
                         last;
                     }
                 }
                 $template_param = undef;
             }
+			$valid_types_local{"$template_param"} = undef;
         }
+
         # skip templates of 2 params 
-        if(/template<typename (\w*), typename (\w*)>/) {
+        elsif(/template<typename (\w*), typename ($varregex)>/) {
             while(<inputfile>) {
-                if(/};/) {
+                if(/^}.*;/) {
                     last;
                 }
             }
         }
 
-
         # a specialized class
-        if(/(class|struct) (\w*)<(\w*)>/) {
-            $non_trivial = 0;
+        if(/(class|struct) ($varregex)<($varregex)>/) {
             $struct_name = $2;
             $param_name = $3;
             $valid_types{"$struct_name<$param_name>"} = undef;
-            $valid_types_regex = '(' . join('|',keys %valid_types) . ')';
- 
-            # eat the { on the next line
-            my $line = <inputfile>;
-            while(<inputfile>) {
-        
-                # class static const variable, print it out
-                if(/static const\s$valid_types_regex (\w*) =/) {
-                    print "\t{ L\"$struct_name<$param_name>\::$2\", $struct_name<$param_name>\::$2 },\n";
-                }
-
-                # break on "};", ending the struct
-                if(/};/) {
-                    $template_param = undef;
-                    last;
-                }
-            }
+			$valid_types_local{"$param_name"} = undef;
        }
        # non specialized class
-       elsif(/(class|struct) (\w*)/) {
-            $non_trivial = 1;
+       elsif(/(class|struct) ($varregex)/) {
             $struct_name = $2;
             if(defined($template_param)) {
                 $valid_types{"$struct_name<ulonglong>"} = undef;
                 $valid_types{"$struct_name<ulong>"} = undef;
+				$valid_types{"$struct_name<T>"} = undef;
             } else {
                 $valid_types{$struct_name} = undef;
             }
-            $valid_types_regex = '(' . join('|',keys %valid_types) . ')';
+        }
 
-            # eat the { on the next line
+		# process a struct/class definition
+		if($struct_name) {
+            
+            if(defined($template_param)) {
+				print "\n\t// struct $struct_name<$template_param>\n";
+            }   
+			elsif(defined($param_name)) {
+				print "\n\t// struct $struct_name<$param_name>\n";
+			}
+            else {
+                print "\n\t// struct $struct_name\n";
+            }
+            $non_trivial = 0;
+
+		    # eat the { on the next line
             my $line = <inputfile>;
+
+			# process the body of the struct
             while(<inputfile>) {
+				
+				# regenerate valid types regex, considering local types
+				if(keys %valid_types_local) {
+					$valid_types_regex = '(' . join('|',keys %valid_types) . '|' . join('|',keys %valid_types_local) . ')';
+				} else {
+					$valid_types_regex = '(' . join('|',keys %valid_types) . ')';
+				}
+
+				# typedef, update valid types (local)
+				if(/typedef\s$valid_types_regex ($varregex);/) {
+					$valid_types_local{"$2"} = undef;
+				}
 
                 # class static const variable, print it out
-                if(/static const\s$valid_types_regex (\w*) =/) {
- 
+                elsif(/static const\s$valid_types_regex ($varregex) =/) {
                     if(defined($template_param)) {
                         print "\t{ L\"$struct_name<ulonglong>\::$2\", $struct_name<ulonglong>\::$2 },\n";
                         print "\t{ L\"$struct_name<ulong>\::$2\", $struct_name<ulong>\::$2 },\n";
                     }   
+					elsif(defined($param_name)) {
+						print "\t{ L\"$struct_name<$param_name>\::$2\", $struct_name<$param_name>\::$2 },\n";
+					}
                     else {
                         print "\t{ L\"$struct_name\::$2\", $struct_name\::$2 },\n";
                     }
                 }
 
+				# data member, print offset
+				elsif(/$valid_types_regex ($varregex);/) {
+					if(defined($template_param)) {
+						print "\t{ L\"offsetof($struct_name<ulonglong>, $2)\", offsetof($struct_name<ulonglong>, $2) },\n";
+						print "\t{ L\"offsetof($struct_name<ulong>, $2)\", offsetof($struct_name<ulong>, $2) },\n";
+					}
+					elsif(defined($param_name)) {
+						print "\t{ L\"offsetof($struct_name<$param_name>, $2)\", offsetof($struct_name<$param_name>, $2) },\n";
+					}
+					else {
+						print "\t{ L\"offsetof($struct_name, $2)\", offsetof($struct_name, $2) },\n";
+					}
+					$non_trivial = 1;
+				}
+
                 # break on "};", ending the struct
-                if(/};/) {
+                elsif(/^}.*;/) {
                     last;
                 }
             }
 
-            if($non_trivial == 1) {
+			# struct done, print the size
+			if($non_trivial == 1) {
                 if(defined($template_param)) {
                     print "\t{ L\"sizeof($struct_name<ulonglong>)\", sizeof($struct_name<ulonglong>) },\n";
                     print "\t{ L\"sizeof($struct_name<ulong>)\", sizeof($struct_name<ulong>) },\n";
                 }
+				elsif(defined($param_name)) {
+				    print "\t{ L\"sizeof($struct_name<$param_name>)\", sizeof($struct_name<$param_name>) },\n";
+				}
                 else {
                     print "\t{ L\"sizeof($struct_name)\", sizeof($struct_name) },\n";
                 }
             }
             $template_param = undef;
-        }
+			$struct_name = undef;
+			$param_name = undef;
+			for(keys %valid_types_local) {
+				delete $valid_types_local{$_};
+			}
+		}
 
         # enum
-        if(/enum (\w*)/) {
+        if(/enum ($varregex)/) {
             print "\n\t// enum $1\n";
             # eat the { on the next line
             my $line = <inputfile>; 
             while(<inputfile>) {
                 
                 # enum value
-                if(/(\w*)\s=/) {
+                if(/($varregex).*,/) {
                     print "\t{ L\"$1\", $1 },\n";
                 }
 
                 # break on "};", ending the enum
-                if(/};/) {
-                    print "\n";
+                if(/^}.*;/) {
                     last;
                 }
             }
