dd15459d5722d76a7c7f9d626df85507c1d91271
terrymah <terry.mahaffey@gmail.com>
2011-11-27 08:01:51 -0800

    (by emk)
    In e-discovery applications, messages will often be referred to by a
    MAPI PR_ENTRYID.  This is a computed property, as described here:
    
      http://pstsdk.codeplex.com/Thread/View.aspx?ThreadId=215111
    
    This patch adds support for computing PR_ENTRYID for top-level messages
    and folders.  As suggested by Terry Mahaffey, all computations are done
    in the PST layer.
    
    All of the PR_ENTRYID values in the test suite were generated using
    MFCMAPI, so we're comparing the computed values against known-good
    values.
    
    This patch does not include the corresponding functions for looking up
    messages and folders using their PR_ENTRYID values.  While these would
    be highly desirable, the current patch stands on its own.
    
    According to Terry Mahaffey, OST files have a different and undocumented
    algorithm for calculating PR_ENTRYID.  This patch adds an 'is_pst'
    function to the database_context interface to detect this case, and
    throws an error.  Note that it would be relatively easy to export the
    database_type value directly (instead of a less-general 'is_pst')
    function, but that this would require including disk.h into
    database_iface.h and exposing the disk-level database_type enumeration
    directly.

--- fairport/ndb/node.h
+++ fairport/ndb/node.h
@@ -98,7 +98,7 @@ public:
 
     //! \brief Tells you if this is a subnode
     //! \returns true if this is a subnode, false otherwise
-    bool is_subnode() { return m_pcontainer_node; }
+    bool is_subnode() const { return m_pcontainer_node; }
 
     //! \brief Returns the data block associated with this node
     //! \returns A shared pointer to the data block
@@ -336,7 +336,7 @@ public:
     //! \copydoc node_impl::get_parent_id()
     node_id get_parent_id() const { return m_pimpl->get_parent_id(); } 
     //! \copydoc node_impl::is_subnode()
-    bool is_subnode() { return m_pimpl->is_subnode(); } 
+    bool is_subnode() const { return m_pimpl->is_subnode(); } 
 
     //! \copydoc node_impl::get_data_block()
     std::tr1::shared_ptr<data_block> get_data_block() const
